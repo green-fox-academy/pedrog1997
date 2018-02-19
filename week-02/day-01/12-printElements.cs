998725d4dfadd72f09d950266aab3379f063523d
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-02-19 13:32:34 +0100

    Print elements excercise

---
+++ week-02/day-01/12-printElements.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace PrintAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `af`
+            //   with the following content: `[4, 5, 6, 7]`
+            // - Print all the elements of `af`
+
+            int[] af = { 4, 5, 6, 7 };
+
+            for (int i = 0; i < af.Length; i++)
+            {
+                Console.WriteLine(af[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
