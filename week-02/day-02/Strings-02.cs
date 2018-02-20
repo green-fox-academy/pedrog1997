75d1dc3720e91a60fc18a5841b5498d0906fdbe4
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-02-20 20:51:22 +0100

    Correct url

--- week-02/day-02/Strings-02.cs
+++ week-02/day-02/Strings-02.cs
@@ -11,7 +11,8 @@ namespace UrlFixer
             // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
             // Also, the URL is missing a crutial component, find out what it is and insert it too!
             // Try to solve it more than once using different string functions!
-
+            
+            url = url.Insert(5, ":");
             url = url.Replace("bots", "odds");
 
             Console.WriteLine(url);
