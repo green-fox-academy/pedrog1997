195abff78b1e561db81052cc3d5917323c389b6d
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-02-21 11:50:04 +0100

    Drawing 01-Line in the middle

---
+++ week-02/day-03/01-lineInTheMiddle/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            // draw a red horizontal line to the canvas' middle.
+            // draw a green vertical line to the canvas' middle.
+
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(0, (canvas.Height / 2), canvas.Width, (canvas.Height/2));
+
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(canvas.Width / 2, 0, canvas.Width / 2, canvas.Height);
+        }
+    }
+}
