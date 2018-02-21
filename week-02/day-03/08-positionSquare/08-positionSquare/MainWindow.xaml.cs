0655a443dea5c4404c9109d4c06d57db9d6937b2
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-02-21 16:27:38 +0100

    Drawings 08 Position Square

---
+++ week-02/day-03/08-positionSquare/08-positionSquare/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace _08_positionSquare
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // create a square drawing function that takes 2 parameters:
+            // the x and y coordinates of the square's top left corner
+            // and draws a 50x50 square from that point.
+            // draw 3 squares with that function.
+            // avoid code duplication.   
+
+            int[] x = { 100, 200, 300 };
+            int y = 150;
+
+            for (int i = 0; i < 3; i++)
+            {
+                squareDrawing(x[i], y, canvas);
+            }
+        }
+
+        public static void squareDrawing(double x, double y, Canvas canvas)
+        {
+            var square = new Rectangle()
+            {
+                Width = 50,
+                Height = 50,
+                Stroke = Brushes.Black
+            };
+            Canvas.SetLeft(square, x);
+            Canvas.SetTop(square, y);
+            canvas.Children.Add(square);
+        }
+    }
+}
