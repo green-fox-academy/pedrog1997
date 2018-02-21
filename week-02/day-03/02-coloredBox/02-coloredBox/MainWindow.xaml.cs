6d6202a4d477b717100e916590a6b739611166d1
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-02-21 12:16:04 +0100

    Drawings - 02 Colored box

---
+++ week-02/day-03/02-coloredBox/02-coloredBox/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace _02_coloredBox
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // Draw a box that has different colored lines on each edge. 
+
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(100, 100, canvas.Width - 100, 100);
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(canvas.Width - 100, 100, canvas.Width - 100, canvas.Height - 100);
+            foxDraw.StrokeColor(Colors.Purple);
+            foxDraw.DrawLine(canvas.Width - 100, canvas.Height - 100, 100, canvas.Height - 100);
+            foxDraw.StrokeColor(Colors.Plum);
+            foxDraw.DrawLine(100, canvas.Height - 100, 100, 100);
+        }
+    }
+}
