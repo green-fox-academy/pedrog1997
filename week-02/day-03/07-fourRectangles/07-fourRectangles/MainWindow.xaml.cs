df9532531fe475f82bd5fc54314d060b86cf75de
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-02-21 16:19:07 +0100

    Drawings 07 Four Rectangles

---
+++ week-02/day-03/07-fourRectangles/07-fourRectangles/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace _07_fourRectangles
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // draw four different size and color rectangles.
+            // avoid code duplication.
+
+            SolidColorBrush[] colors = new SolidColorBrush[] { Brushes.Black, Brushes.Blue, Brushes.Green, Brushes.Indigo };
+            int[] position = { 0, 75, 200, 400 };
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                var rectangle = new Rectangle()
+                {
+                    //Stroke = Brushes.Black,
+                    Height = 50*i+35,
+                    Width = 100*i+20,
+                    Fill = colors[i],
+                };
+                Canvas.SetLeft(rectangle, position[i]);
+                Canvas.SetTop(rectangle, position[i]);
+                canvas.Children.Add(rectangle);
+
+            }
+        }
+    }
+}
