74f1a4e57ef75a8df1d18ea02e53fd03c2fe686e
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-02-22 09:55:14 +0100

    Drawings 12 Purple Steps 3d

---
+++ week-02/day-03/12-PurpleSteps3d/12-PurpleSteps3d/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace _12_PurpleSteps3d
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            double x = 20;
+            double size = 20;
+
+            for (int i = 0; i < 6; i++)
+            {
+                SquareDrawing(x, size, canvas);
+                x += size;
+                size += 20;
+            }
+        }
+
+        public static void SquareDrawing(double x, double size, Canvas canvas)
+        {
+            var square = new Rectangle()
+            {
+                Height = size,
+                Width = size,
+                Stroke = Brushes.Purple,
+                Fill = Brushes.Purple
+            };
+            Canvas.SetLeft(square, x);
+            Canvas.SetTop(square, x);
+            canvas.Children.Add(square);
+        }
+    }
+}
