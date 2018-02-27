f91bf85c4393cf47a43eecb0757670ca1f3a2d9e
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-02-27 12:36:44 +0100

    Write Single Line excercise

---
+++ week-03/day-02/04-WriteSingleLine/04-WriteSingleLine/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace _04_WriteSingleLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "my-file.txt";
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(fileName))
+                {
+                    writer.WriteLine("Pedro Gómez de la Garza");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Unable to write on file.");
+            }
+
+            Console.Read();
+        }
+    }
+}
