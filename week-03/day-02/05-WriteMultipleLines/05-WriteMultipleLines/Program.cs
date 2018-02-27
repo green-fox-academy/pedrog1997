b12c57dc566998b0ec371a764a032a212f96c603
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-02-27 14:51:43 +0100

    Write Multiple Lines excercise

---
+++ week-03/day-02/05-WriteMultipleLines/05-WriteMultipleLines/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace _05_WriteMultipleLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "text.txt";
+            WriteLines(path, "pear", 5);
+
+        }
+
+        static void WriteLines(string path, string word, int numberOfLines)
+        {
+            try
+            {
+                string[] wordArray = new string[numberOfLines];
+                for (int i = 0; i < wordArray.Length; i++)
+                {
+                    wordArray[i] = word;
+                }
+                File.WriteAllLines(path, wordArray);
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+    }
+}
