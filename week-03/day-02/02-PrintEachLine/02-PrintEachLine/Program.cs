6a7ca3a7a84e4b537ed52a4b8a8682a2b86069fd
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-02-27 11:50:51 +0100

    Print each line excercise

---
+++ week-03/day-02/02-PrintEachLine/02-PrintEachLine/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace _02_PrintEachLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Directory.GetCurrentDirectory();
+
+            try
+            {
+                string[] fileLines = File.ReadAllLines("my-files.txt");
+                foreach (var lines in fileLines)
+                {
+                    Console.WriteLine(lines);
+                }
+            }
+            catch (Exception error)
+            {
+                Console.Error.WriteLine("Unable to read file.");
+            }
+
+            Console.Read();
+        }
+    }
+}
