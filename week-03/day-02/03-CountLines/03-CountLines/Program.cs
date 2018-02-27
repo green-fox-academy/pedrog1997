3185ffbffa80e71ddbef5417184ee714489a8d83
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-02-27 12:07:54 +0100

    Count Lines excercise

---
+++ week-03/day-02/03-CountLines/03-CountLines/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace _03_CountLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "files.txt";
+
+            Console.WriteLine(CountLines(fileName));
+
+            Console.Read();
+        }
+
+        static int CountLines(string path)
+        {
+            try
+            {
+                string[] fileLines = File.ReadAllLines(path);
+                return fileLines.Length;
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }
+        }
+    }
+}
