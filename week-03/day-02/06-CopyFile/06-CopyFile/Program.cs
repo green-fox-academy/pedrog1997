73c4657fa348dad0b0b098fa47e153af76b08244
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-02-27 15:56:22 +0100

    Copy File and TicTacToe excercises

---
+++ week-03/day-02/06-CopyFile/06-CopyFile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace _06_CopyFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileToCopy = "fileToCopy.txt";
+            string destinationFile = "destinationFile.txt";
+
+            CopyFile(fileToCopy, destinationFile);
+        }
+
+        static bool CopyFile(string fileToCopy, string destinationFile)
+        {
+            try
+            {
+                string[] arrayFileToCopy = File.ReadAllLines(fileToCopy);
+                File.WriteAllLines(destinationFile, arrayFileToCopy);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+                Console.Read();
+                return false;
+            }
+        }
+    }
+}
---
+++ week-03/day-02/07-TicTacToe/07-TicTacToe/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+
+namespace _07_TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TicTacToe("winnerX.txt"));
+
+            Console.Read();
+        }
+
+        static string TicTacToe(string fileName)
+        {
+            try
+            {
+                string[] linesTicTacToe = File.ReadAllLines(fileName);
+                char[] firstLine = linesTicTacToe[0].ToCharArray();
+                char[] secondLine = linesTicTacToe[1].ToCharArray();
+                char[] thirdLine = linesTicTacToe[2].ToCharArray();
+
+                string winner;
+
+                if (firstLine[0] == firstLine[1] && firstLine[1] == firstLine[2])
+                {
+                    winner = firstLine[0].ToString();
+                }
+                else if (secondLine[0] == secondLine[1] && secondLine[1] == secondLine[2])
+                {
+                    winner = secondLine[0].ToString();
+                }
+                else if (thirdLine[0] == thirdLine[1] && thirdLine[1] == thirdLine[2])
+                {
+                    winner = thirdLine[0].ToString();
+                }
+                else if (firstLine[0] == secondLine[0] && secondLine[0] == thirdLine[0])
+                {
+                    winner = thirdLine[0].ToString();
+                }
+                else if (firstLine[1] == secondLine[1] && secondLine[1] == thirdLine[1])
+                {
+                    winner = thirdLine[1].ToString();
+                }
+                else if (firstLine[2] == secondLine[2] && secondLine[2] == thirdLine[2])
+                {
+                    winner = thirdLine[2].ToString();
+                }
+                else if (firstLine[0] == secondLine[1] && secondLine[1] == thirdLine[2])
+                {
+                    winner = firstLine[0].ToString();
+                }
+                else if(firstLine[2] == secondLine[1] && secondLine[1] == thirdLine[0])
+                {
+                    winner = thirdLine[0].ToString();
+                }
+                else
+                {
+                    winner = "draw";
+                }
+                return winner;
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+                return "";
+            }
+        }
+    }
+}
