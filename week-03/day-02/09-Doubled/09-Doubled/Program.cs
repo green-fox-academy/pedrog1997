ea712b922daeeaac872bb62b9819ae893b16c2dd
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-02-27 17:17:21 +0100

    Doubled characters

---
+++ week-03/day-02/09-Doubled/09-Doubled/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace _09_Doubled
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "duplicated-chars.txt";
+            Console.WriteLine(Decryption(fileName));
+
+            Console.Read();
+        }
+
+        static string Decryption(string fileName)
+        {
+            string[] linesOfFile = File.ReadAllLines(fileName);
+
+            for (int i = 0; i < linesOfFile.Length; i++)
+            {
+                for (int j = 1; j < linesOfFile[i].Length; j++)
+                {
+                    if (linesOfFile[i][j] == linesOfFile[i][j-1])
+                    {
+                        linesOfFile[i] = linesOfFile[i].Remove(j, 1);
+                        j--;
+                    }
+                }
+            }
+            string decrypted = "";
+            for (int i = 0; i < linesOfFile.Length; i++)
+            {
+                decrypted += linesOfFile[i] + "\n";
+            }
+            return decrypted;
+        }
+    }
+}
