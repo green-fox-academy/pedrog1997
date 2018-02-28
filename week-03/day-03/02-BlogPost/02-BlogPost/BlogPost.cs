ed2c7923221657e415a152d81c6e2e9188f2bac8
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-02-28 13:40:34 +0100

    Blog Post excercise

---
+++ week-03/day-03/02-BlogPost/02-BlogPost/BlogPost.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02_BlogPost
+{
+    class BlogPost
+    {
+        public string AuthorName;
+        public string Title;
+        public string Text;
+        public string PublicationDate;
+
+    }
+}
---
+++ week-03/day-03/02-BlogPost/02-BlogPost/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _02_BlogPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlogPost LoremIpsum = new BlogPost();
+            LoremIpsum.AuthorName = "John Doe";
+            LoremIpsum.Title = "Lorem Ipsum";
+            LoremIpsum.Text = "Lorem ipsum dolor sit amet";
+            LoremIpsum.PublicationDate = "2000.05.04.";
+
+            BlogPost WaitButWhy = new BlogPost();
+            WaitButWhy.AuthorName = "Tim Urban";
+            WaitButWhy.Title = "Wait But Why";
+            WaitButWhy.Text = "A popular long-form, stick-figure-illustrated blog about almost everything.";
+            WaitButWhy.PublicationDate = "2010.10.10.";
+
+            BlogPost OneEngineer = new BlogPost();
+            OneEngineer.AuthorName = "William Turton";
+            OneEngineer.Title = "One Engineer Is Trying to Get IBM to Reckon With Trump";
+            OneEngineer.Text = "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.";
+            OneEngineer.PublicationDate = "2017.03.28.";
+        }
+    }
+}
