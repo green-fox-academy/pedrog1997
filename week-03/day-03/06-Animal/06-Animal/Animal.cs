f790702ffcd33de928d5ad941a83ecfb277f82cc
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-02-28 16:54:36 +0100

    Animal excercise

---
+++ week-03/day-03/06-Animal/06-Animal/Animal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06_Animal
+{
+    public class Animal
+    {
+        private int hunger = 50;
+        private int thirst = 50;
+
+        public Animal()
+        {
+            Console.WriteLine("Your animal has a hunger value of " + hunger + " and a thirst value of " + thirst);
+        }
+
+        public void SetHunger(int hunger)
+        {
+            this.hunger = hunger;
+        }
+
+        public void SetThirst(int thirst)
+        {
+            this.thirst = thirst;
+        }
+
+        public void Eat()
+        {
+            hunger--;
+            Console.WriteLine("After eating, your animal has a hunger value of " + hunger);
+        }
+
+        public void Drink()
+        {
+            thirst--;
+            Console.WriteLine("After drinking, your animal has a thirst value of " + thirst);
+        }
+
+        public void Play()
+        {
+            hunger++;
+            thirst++;
+            Console.WriteLine("After playing, your animal has a hunger value of " + hunger + " and a thirst value of " + thirst);
+        }
+    }
+}
---
+++ week-03/day-03/06-Animal/06-Animal/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _06_Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var animal = new Animal();
+            animal.Play();
+            animal.Eat();
+            animal.Drink();
+
+            Console.Read();
+        }
+    }
+}
