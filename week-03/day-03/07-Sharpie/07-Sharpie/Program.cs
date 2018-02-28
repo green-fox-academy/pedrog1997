71b43f4c9c3022159f18f10a8455eacca6484b2e
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-02-28 17:11:05 +0100

    Sharpie excercise

---
+++ week-03/day-03/07-Sharpie/07-Sharpie/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _07_Sharpie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sharpie = new Sharpie("orange", 50f);
+            sharpie.Use();
+            sharpie.Use();
+
+            Console.Read();
+
+        }
+    }
+}
