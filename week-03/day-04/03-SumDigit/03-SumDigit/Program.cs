1d6f672cc71d77418fa9f4bed64fbbab905c3dd4
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-03-01 11:01:18 +0100

    Sum of all digits excercise

---
+++ week-03/day-04/03-SumDigit/03-SumDigit/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _03_SumDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input for sum of all digits: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(SumDigit(n));
+
+            Console.Read();
+        }
+
+        static int SumDigit(int n)
+        {
+            if (n % 10 == n)
+            {
+                return n;
+            }
+            else
+            {
+                return (n % 10) + SumDigit(n / 10);
+            }
+        }
+    }
+}
