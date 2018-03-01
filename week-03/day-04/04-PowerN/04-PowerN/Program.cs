f59ebd2069ff53d3aa0952e40d5e5eb513e7b29d
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-03-01 11:07:40 +0100

    Base to the n power excercises

---
+++ week-03/day-04/04-PowerN/04-PowerN/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _04_PowerN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input base:");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input exponent:");
+            int exponent = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(PowerN(number, exponent));
+
+            Console.Read();
+        }
+
+        static int PowerN(int number, int exponent)
+        {
+            if (exponent == 1)
+            {
+                return number;
+            }
+            else
+            {
+                return number *= PowerN(number, exponent - 1);
+            }
+        }
+    }
+}
