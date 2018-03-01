96a2a0052ed3cc554126aaccef7715ba970a7ffc
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-03-01 11:12:55 +0100

    Number of ears of bunnies excercise

---
+++ week-03/day-04/05-Bunnies/05-Bunnies/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05_Bunnies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number of bunnies:");
+            int numberOfBunnies = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(BunniesEars(numberOfBunnies));
+
+            Console.Read();
+        }
+
+        static int BunniesEars(int numberOfBunnies)
+        {
+            if (numberOfBunnies == 1)
+            {
+                return 2;
+            }
+            else
+            {
+                return 2 + BunniesEars(numberOfBunnies - 1);
+            }
+        }
+    }
+}
