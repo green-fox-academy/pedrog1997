1a06e152faa15b1325be83c6aa6fbd735b726a00
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-03-01 11:19:20 +0100

    Mutated bunnies excercise

---
+++ week-03/day-04/06-BunniesAgain/06-BunniesAgain/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _06_BunniesAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number of bunnies:");
+            int numberOfBfunnies = int.Parse(Console.ReadLine());
+            Console.WriteLine(MutatedBunnies(numberOfBfunnies));
+
+            Console.Read();
+        }
+
+        static int MutatedBunnies(int numberOfBunnies)
+        {
+            if (numberOfBunnies == 1)
+            {
+                return 2;
+            }
+            else
+            {
+                if (numberOfBunnies % 2 == 0)
+                {
+                    return 3 + MutatedBunnies(numberOfBunnies - 1);
+                }
+                else
+                {
+                    return 2 + MutatedBunnies(numberOfBunnies - 1);
+                }
+            }
+        }
+    }
+}
