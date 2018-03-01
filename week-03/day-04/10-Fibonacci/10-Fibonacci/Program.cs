dbc69b6ec994b33167d27a6af840652c7c111b89
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-03-01 11:53:03 +0100

    Fibonacci sequence excercise

---
+++ week-03/day-04/10-Fibonacci/10-Fibonacci/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _10_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("nth position of the Fibonacci sequence");
+            int nthFibonacci = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Fibonacci(nthFibonacci));
+
+            Console.Read();
+        }
+
+        static int Fibonacci(int nthFibonacci)
+        {
+            if (nthFibonacci == 0)
+            {
+                return 0;
+            }
+            else if (nthFibonacci == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonacci(nthFibonacci - 1) + Fibonacci(nthFibonacci - 2);
+            }
+        }
+    }
+}
