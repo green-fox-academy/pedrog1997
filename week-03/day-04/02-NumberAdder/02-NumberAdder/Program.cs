20c9e49e525555816bc6d3c1b133b0b75a4f5ecb
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-03-01 10:54:33 +0100

    Number Adder excercise

---
+++ week-03/day-04/02-NumberAdder/02-NumberAdder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _02_NumberAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input for number adder: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(NumberAdder(n));
+
+            Console.Read();
+        }
+
+        static int NumberAdder(int n)
+        {
+            int sum = n;
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+            sum += NumberAdder(n - 1);
+            return sum;
+            }
+        }
+    }
+}
