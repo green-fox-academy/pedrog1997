baa0a77f68e152f5f47dd5189c44730b6545b4aa
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-03-01 11:39:34 +0100

    Strings Again excercise. Remove all x

---
+++ week-03/day-04/08-StringsAgain/08-StringsAgain/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _08_StringsAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input Word");
+            string word = Console.ReadLine();
+
+            Console.WriteLine(RemoveX(word));
+
+            Console.Read();
+        }
+
+        static string RemoveX(string word)
+        {
+            if (word.Length == 1)
+            {
+                if (word == "x")
+                {
+                    return string.Empty;
+                }
+                else
+                {
+                    return word;
+                }
+            }
+            else
+            {
+                if (word[0] == 'x')
+                {
+                    return RemoveX(word.Substring(1));
+                }
+                else
+                {
+                    return word[0] + RemoveX(word.Substring(1));
+                }
+            }
+        }
+    }
+}
