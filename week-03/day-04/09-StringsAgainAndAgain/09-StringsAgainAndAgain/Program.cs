98fdf39911817d10486db9c6bd2fbcca97bc5fe3
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-03-01 11:45:07 +0100

    Strings again and again excercise. Separate chars with *

---
+++ week-03/day-04/09-StringsAgainAndAgain/09-StringsAgainAndAgain/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _09_StringsAgainAndAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input Word");
+            string word = Console.ReadLine();
+
+            Console.WriteLine(SeparateChars(word));
+
+            Console.Read();
+        }
+
+        static string SeparateChars(string word)
+        {
+            if (word.Length == 2)
+            {
+                return word[0] + "*" + word[1];
+            }
+            else
+            {
+                return word[0] + "*" + SeparateChars(word.Substring(1));
+            }
+        }
+    }
+}
