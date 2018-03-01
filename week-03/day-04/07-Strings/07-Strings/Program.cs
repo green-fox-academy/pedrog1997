07d03183954682f31583248bea94ba861cc7105f
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-03-01 11:32:09 +0100

    Strings excercise

---
+++ week-03/day-04/07-Strings/07-Strings/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _07_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input word: ");
+            string word = Console.ReadLine();
+
+            Console.WriteLine(ConvertXToY(word));
+
+            Console.Read();
+        }
+
+        static string ConvertXToY(string word)
+        {
+            if (word.Length == 1)
+            {
+                if (word == "x")
+                {
+                    return "y";
+                }
+                else
+                {
+                    return word;
+                }
+            }
+            else
+            {
+                if (word[0] == 'x')
+                {
+                    return word = "y" + ConvertXToY(word.Substring(1));
+                }
+                else
+                {
+                    return word = word[0] + ConvertXToY(word.Substring(1));
+                }
+            }
+        }
+    }
+}
