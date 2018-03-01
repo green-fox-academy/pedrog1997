96f9956e7e07075feb0c7ed2b675d24a8400d6ef
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-03-01 11:56:43 +0100

    Refactorio excercise

---
+++ week-03/day-04/11-Refactorio/11-Refactorio/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _11_Refactorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input Number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Refactorio(number));
+
+            Console.Read();
+        }
+
+        static int Refactorio(int number)
+        {
+            if (number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Refactorio(number - 1);
+            }
+        }
+    }
+}
