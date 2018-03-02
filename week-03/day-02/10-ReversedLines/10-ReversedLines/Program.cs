cfb8058c8a75641c3f0d0b2bca9774747fe0f6e7
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-03-02 10:56:59 +0100

    Reversed Lines excercise

--- week-03/day-02/10-ReversedLines/10-ReversedLines/Program.cs
+++ week-03/day-02/10-ReversedLines/10-ReversedLines/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace _10_ReversedLines
 {
@@ -7,6 +8,23 @@ namespace _10_ReversedLines
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string[] text = File.ReadAllLines("reversedLines.txt");
+            string[] newText = new string[text.Length];
+            for (int i = 0; i < text.Length; i++)
+            {
+                newText[i] = ReverseLine(text[i]);
+            }
+            File.WriteAllLines("correctText.txt", newText);
+        }
+
+        static string ReverseLine(string line)
+        {
+            string newLine = string.Empty;
+            for (int i = line.Length - 1; i >= 0; i--)
+            {
+                newLine += line[i];
+            }
+            return newLine;
         }
     }
 }
