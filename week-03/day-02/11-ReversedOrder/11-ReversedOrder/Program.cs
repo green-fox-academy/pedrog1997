f5c39e21f73ff6d75773771863ad780306e783df
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-03-02 11:08:37 +0100

    Reversed Order excercise

---
+++ week-03/day-02/11-ReversedOrder/11-ReversedOrder/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace _11_ReversedOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllLines("correctText.txt", ReversedOrder("reversedOrder.txt"));
+        }
+
+        static string[] ReversedOrder(string path)
+        {
+            string[] text = File.ReadAllLines(path);
+            string[] newText = new string[text.Length];
+            int j = 0;
+
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                newText[j] = text[i];
+                j++;
+            }
+
+            return newText;
+        }
+    }
+}
