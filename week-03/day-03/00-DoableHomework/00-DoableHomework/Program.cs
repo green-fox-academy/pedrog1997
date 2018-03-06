7f9e49e868fb72189c352885b1e645a3fa6370f8
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-03-06 16:40:57 +0100

    Doable Homework before classes added

---
+++ week-03/day-03/00-DoableHomework/00-DoableHomework/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _00_DoableHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> tree1 = new List<object>()
+            {
+                "Maple",
+                "Green",
+                100,
+                "Male"
+            };
+
+            List<object> tree2 = new List<object>()
+            {
+                "Oak",
+                "Brown",
+                50,
+                "Male"
+            };
+
+            List<object> tree3 = new List<object>()
+            {
+                "Dry",
+                "Gray",
+                150,
+                "Female"
+            };
+
+            List<object> tree4 = new List<object>()
+            {
+                "Invisible Tree",
+                "Unknown",
+                8,
+                "Asexual"
+            };
+
+            List<object> tree5 = new List<object>()
+            {
+                "Magical Bean Tree",
+                "Green",
+                0,
+                "Whatever"
+            };
+        }
+    }
+}
