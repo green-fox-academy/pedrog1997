5e07b8167f1d4920ffdfafef316574b735a2f09c
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-03-06 17:06:54 +0100

    Added the Sharpie Set excercise

---
+++ week-03/day-03/11-SharpieSet/11-SharpieSet/Sharpie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _11_SharpieSet
+{
+    class Sharpie
+    {
+        private string color;
+        private float width;
+        private float inkAmount = 100f;
+
+        public Sharpie(string color, float width)
+        {
+            this.color = color;
+            this.width = width;
+            Console.WriteLine("Your sharpie has an ink amount value of " + inkAmount);
+        }
+
+        public void SetSharpie(float inkAmount)
+        {
+            this.inkAmount = inkAmount;
+        }
+
+        public void Use()
+        {
+            inkAmount--;
+            Console.WriteLine("After use, your sharpie has an ink amount value of " + inkAmount);
+        }
+
+        public float GetInkAmount()
+        {
+            return inkAmount;
+        }
+    }
+}
---
+++ week-03/day-03/11-SharpieSet/11-SharpieSet/SharpieSet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _11_SharpieSet
+{
+    class SharpieSet
+    {
+        private List<Sharpie> sharpies = new List<Sharpie>();
+
+        public int CountUsable()
+        {
+            int count = 0;
+            foreach (var sharpie in sharpies)
+            {
+                if (sharpie.GetInkAmount() > 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public void RemoveTrash()
+        {
+            for (int i = 0; i < sharpies.Count; i++)
+            {
+                if (sharpies[i].GetInkAmount() <= 0)
+                {
+                    sharpies.RemoveAt(i);
+                    i--;
+                }
+            }
+        }
+    }
+}
