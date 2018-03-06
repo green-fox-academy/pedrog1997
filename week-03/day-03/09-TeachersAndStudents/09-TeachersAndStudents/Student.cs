de4c9bd0b6edeb146789a9e4da790999089002a3
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-03-06 16:18:40 +0100

    Added the excercise of Teachers and Students

---
+++ week-03/day-03/09-TeachersAndStudents/09-TeachersAndStudents/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09_TeachersAndStudents
+{
+    class Student
+    {
+        public void Learn()
+        {
+
+        }
+
+        public void Question(Teacher teacher)
+        {
+            teacher.Answer();
+        }
+    }
+}
---
+++ week-03/day-03/09-TeachersAndStudents/09-TeachersAndStudents/Teacher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09_TeachersAndStudents
+{
+    class Teacher
+    {
+        public void Answer()
+        {
+
+        }
+
+        public void Teach(Student student)
+        {
+            student.Learn();
+        }
+    }
+}
