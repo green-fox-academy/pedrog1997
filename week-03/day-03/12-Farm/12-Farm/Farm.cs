29586bce302f38186082b4aca404c634e37b3133
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-03-06 17:26:39 +0100

    Added the farm excercise

---
+++ week-03/day-03/12-Farm/12-Farm/Farm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _12_Farm
+{
+    class Farm
+    {
+        private List<Animal> animals = new List<Animal>();
+        private int slots = 10;
+
+        public Farm()
+        {
+            animals.Capacity = slots;
+        }
+
+        public void Breed()
+        {
+            if (animals.Count < slots)
+            {
+                animals.Add(new Animal());
+            }
+        }
+
+        public void Slaughter()
+        {
+            int leastHungry = 0;
+
+            for (int i = 0; i < animals.Count; i++)
+            {
+                if (animals[leastHungry].GetHunger() < animals[i].GetHunger())
+                {
+                    leastHungry = i;
+                }
+            }
+
+            animals.RemoveAt(leastHungry);
+        }
+
+    }
+}
