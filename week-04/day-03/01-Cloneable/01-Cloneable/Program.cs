aef3d67e0266a0d4a1e5215f4acc7c5ab24abf6d
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-03-07 15:28:24 +0100

    Cloneable excercise

---
+++ week-04/day-03/01-Cloneable/01-Cloneable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01_Cloneable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new Student("John", 20, "male", "BME");
+
+            var student02 = student.Clone() as Student;
+            student02.SkipDays(5);
+
+            student.Introduce();
+            student02.Introduce();
+
+            Console.Read();
+        }
+    }
+}
---
+++ week-04/day-03/01-Cloneable/01-Cloneable/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_Cloneable
+{
+    class Student : ICloneable
+    {
+        private string name = "Jane Doe";
+        private int age = 30;
+        private string gender = "female";
+        private string previousOrganization = "The school of life";
+        private int skippedDays = 0;
+
+        public Student()
+        {
+
+        }
+
+        public Student(string name, int age, string gender, string previousOrganization)
+        {
+            this.previousOrganization = previousOrganization;
+        }
+
+        public object Clone()
+        {
+            return new Student(name, age, gender, previousOrganization);
+        }
+
+        public void GetGoal()
+        {
+            Console.WriteLine("My goal is : be a junior software developer.");
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine("Hi, I am {0}, a {1} years old {2} from {3} who skipped {4} days from the course already.",
+                name, age, gender, previousOrganization, skippedDays);
+        }
+
+        public void SkipDays(int numberOfDays)
+        {
+            skippedDays += numberOfDays;
+        }
+    }
+}
