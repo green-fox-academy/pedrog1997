59b703c4e485a9e457e12724f005e913e6ef2238
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-03-26 15:02:38 +0200

    feat(linq): Ecxercise 09

---
+++ week-06/day-01/Link-09/Link-09/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Link_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] letters = { 'P', 'e', 'd', 'r', 'o' };
+
+            var lettersToWord = string.Join("", letters);
+
+            Console.WriteLine("Query syntax");
+            Console.WriteLine(lettersToWord);
+
+            Console.Read();
+        }
+    }
+}
