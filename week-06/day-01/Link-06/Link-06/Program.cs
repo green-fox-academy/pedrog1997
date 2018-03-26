9dc3ed4429148f0e05078d016024a3a1fff03f0e
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-03-26 14:18:11 +0200

    feat(linq): Ecxercise 06

---
+++ week-06/day-01/Link-06/Link-06/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Link_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string longWord = "megszentségteleníthetetlenségeskedéseitekért";
+
+            var frecuencyOfLettersQuery = from letter in longWord.ToCharArray()
+                                          group letter by letter into letters
+                                          orderby letters.Count() descending
+                                          select new { letter = letters.Key, frecuency = letters.Count() };
+            Console.WriteLine("Query syntax: ");
+            foreach (var letter in frecuencyOfLettersQuery)
+            {
+                Console.WriteLine($"Letter: {letter.letter} Frecuency: {letter.frecuency}");
+            }
+
+            var frecuencyOfLettersMethod = longWord.ToCharArray().GroupBy(letter => letter).OrderByDescending(letter => letter.Count());
+
+            Console.WriteLine("Method syntax: ");
+            foreach (var letter in frecuencyOfLettersMethod)
+            {
+                Console.WriteLine($"Letter: {letter.Key} Frecuency: {letter.Count()}");
+            }
+
+            Console.Read();
+        }
+    }
+}
