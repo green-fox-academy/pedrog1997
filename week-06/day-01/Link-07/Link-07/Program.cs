f5a50c0bdbb79ead4b57ba467a21e4eac907f232
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-03-26 14:26:35 +0200

    feat(linq): Ecxercise 07

---
+++ week-06/day-01/Link-07/Link-07/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Link_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            var wordsStartAEndIQuery = from word in cities
+                                  where word.StartsWith('A') && word.EndsWith('I')
+                                  select word;
+
+            Console.WriteLine("Query Syntax: ");
+            foreach (var word in wordsStartAEndIQuery)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.WriteLine("Method Syntax:");
+            var wordsStartAEndIMethod = cities.Where(word => word.StartsWith('A') && word.EndsWith('I'));
+            foreach (var word in wordsStartAEndIMethod)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.Read();
+        }
+    }
+}
