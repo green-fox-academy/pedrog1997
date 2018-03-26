925d36c0aa8b7d74d6b1dbca666640f62ec9fb0a
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-03-26 11:48:21 +0200

    feat(linq): Excercise 02

---
+++ week-06/day-01/Linq-02/Linq-02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Linq_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            var avgOddNumbersQuery = (from x in n
+                                 where x % 2 != 0
+                                 select x)
+                                 .Average();
+
+            var avgOddNumbersMethod = n.Where(x => x % 2 != 0).Average();
+
+            Console.WriteLine("With Query: " + avgOddNumbersQuery);
+            Console.WriteLine("With Method: " + avgOddNumbersMethod);
+
+            Console.Read();
+            
+        }
+    }
+}
