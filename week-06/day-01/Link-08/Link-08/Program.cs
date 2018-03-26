823aec8f9dddfeb8f1124d7079143730dd589f58
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-03-26 14:36:52 +0200

    feat(linq): Ecxercise 08

---
+++ week-06/day-01/Link-08/Link-08/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Link_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Pedro Gómez de la Garza";
+
+            var capitalLettersQuery = from letter in name.ToCharArray()
+                                      where letter >= 'A' && letter <= 'Z'
+                                      select letter;
+
+            Console.WriteLine("Query Syntax");
+            foreach (var capitalLetter in capitalLettersQuery)
+            {
+                Console.WriteLine(capitalLetter);
+            }
+
+            var capitalLettersMethod = name.ToCharArray().Where(letter => letter >= 'A' && letter <= 'Z');
+
+            Console.WriteLine("Method Syntax");
+            foreach (var capitalLetter in capitalLettersMethod)
+            {
+                Console.WriteLine(capitalLetter);
+            }
+
+            Console.Read();
+        }
+    }
+}
