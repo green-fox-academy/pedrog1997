25d19ef5dfde7965f6b85f95cc49b9111d431c9b
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-03-26 11:57:59 +0200

    feat(linq): Ecxercise 03

---
+++ week-06/day-01/Linq-03.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Linq_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            var squarredPositivesQuery = from x in n
+                                    where x > 0
+                                    select x * x;
+
+            var squarredPositivesMethod = n.Where(x => x > 0).Select(x => x * x);
+
+            Console.WriteLine("Query syntax: ");
+            foreach (var squarredPositive in squarredPositivesQuery)
+            {
+                Console.WriteLine(squarredPositive);
+            }
+            Console.WriteLine("Method Syntax: ");
+            foreach (var squarredPositive in squarredPositivesMethod)
+            {
+                Console.WriteLine(squarredPositive);
+            }
+
+            Console.Read();
+        }
+    }
+}
