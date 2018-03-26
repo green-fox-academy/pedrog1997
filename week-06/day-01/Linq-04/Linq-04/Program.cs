9735bff8c9dc180ceb3eed2b912a8bdf7174486c
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-03-26 12:10:27 +0200

    feat(linq): Ecxercise 04

---
+++ week-06/day-01/Linq-04/Linq-04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Linq_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new[] { 3, 9, 2, 8, 6, 5 };
+
+            var squaredNumberMoreThan20Query = from x in n
+                                          where x * x > 20
+                                          select x;
+
+            var squaredNumberMoreThan20Method = n.Where(x => x * x > 20);
+
+            Console.WriteLine("Query Syntax ");
+            foreach (var squaredNumber in squaredNumberMoreThan20Query)
+            {
+                Console.WriteLine(squaredNumber);
+            }
+            Console.WriteLine("Mwthod syntax ");
+            foreach (var squaredNumber in squaredNumberMoreThan20Method)
+            {
+                Console.WriteLine(squaredNumber);
+            }
+
+            Console.Read();
+        }
+    }
+}
