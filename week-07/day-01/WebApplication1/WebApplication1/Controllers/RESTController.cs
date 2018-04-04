4ccfca631f654938f40ab5a97191c7c55fc0081c
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-04-04 14:54:45 +0200

    feat: Hello World REST App

---
+++ week-07/day-01/WebApplication1/WebApplication1/Controllers/RESTController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api")]
+    public class RESTController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Greeting")]
+        public IActionResult Greeting()
+        {
+            var greeting = new Greeting()
+            {
+                Id = 1,
+                Content = "Hello World!"
+            };
+
+            return new JsonResult(greeting);
+        }
+    }
+}
