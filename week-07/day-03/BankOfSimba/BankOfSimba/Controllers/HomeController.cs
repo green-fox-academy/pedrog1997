36cdcf7a92b235aca666d644b3a6e4b716b6b2b5
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-04-04 18:29:30 +0200

    feat(BankOfSimba): Part 1

---
+++ week-07/day-03/BankOfSimba/BankOfSimba/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BankOfSimba.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankOfSimba.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("simba")]
+        public IActionResult Simba()
+        {
+            var bankAccountSimba = new BankAccount("Simba", "2000", "Animal.Lion");
+
+            return View();
+        }
+    }
+}
---
+++ week-07/day-03/BankOfSimba/BankOfSimba/Models/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfSimba.Models
+{
+    public class BankAccount
+    {
+        public string Name { get; set; }
+        public string Balance { get; set; }
+        public string AnimalType { get; set; }
+
+        public BankAccount(string name, string balance, string animalType)
+        {
+            Name = name;
+            Balance = balance;
+            AnimalType = animalType;
+        }
+    }
+}
