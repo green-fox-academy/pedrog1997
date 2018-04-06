310f75cf5332a30c611044efe5cebcb20eb8747c
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-04-06 10:46:49 +0200

    DI-Counter

---
+++ week-07/day-04/DI_Counter/DI_Counter/Controllers/CounterController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DI_Counter.Models;
+using DI_Counter.Services;
+
+namespace DI_Counter.Controllers
+{
+    public class CounterController : Controller
+    {
+        private ICounter _counter = new Counter();
+
+        public CounterController(ICounter counter)
+        {
+            _counter = counter;
+        }
+
+        [HttpGet("/index")]
+        public IActionResult Index()
+        {
+            return View(_counter);
+        }
+
+        [HttpPost("/index")]
+        public IActionResult Counter()
+        {
+            _counter.IncreaseNumber();
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ week-07/day-04/DI_Counter/DI_Counter/Models/Counter.cs
@@ -0,0 +1,19 @@
+﻿using DI_Counter.Services;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DI_Counter.Models 
+{
+    public class Counter : ICounter
+    {
+        public int Number { get; set; }
+
+        public void IncreaseNumber()
+        {
+            Number++;
+        }
+    }
+}
