c2ea06bbb089faf96cceaa58bead5ec3d1a956d7
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-04-06 11:09:23 +0200

    DI Greet Someone

---
+++ week-07/day-04/DI_GreetSomeone/DI_GreetSomeone/Controllers/GreetController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DI_GreetSomeone.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DI_GreetSomeone.Controllers
+{
+    public class GreetController : Controller
+    {
+        private INameable _person;
+
+        public GreetController(INameable person)
+        {
+            _person = person;
+        }
+
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            return View(_person);
+        }
+
+        [HttpPost("/")]
+        public IActionResult Index(string name)
+        {
+            _person.Name = name;
+            return RedirectToAction("greet");
+        }
+
+        [HttpGet("/greet")]
+        public IActionResult Greet()
+        {
+            return View(_person);
+        }
+    }
+}
---
+++ week-07/day-04/DI_GreetSomeone/DI_GreetSomeone/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using DI_GreetSomeone.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DI_GreetSomeone.Models
+{
+    public class Person : INameable
+    {
+        public string Name { get; set; }
+    }
+}
