c7ef86dd9e3a9db5708fb3a7a34b62b61c17b182
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-04-10 11:42:18 +0200

    feat(ListToDos): Controller

---
+++ week-08/day-02/ListToDos/ListToDos/Controllers/TodoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ListToDos.Controllers
+{
+    [Route("todo")]
+    public class TodoController : Controller
+    {
+        [Route("list")]
+        public IActionResult List()
+        {
+            return View("List");
+        }
+    }
+}
