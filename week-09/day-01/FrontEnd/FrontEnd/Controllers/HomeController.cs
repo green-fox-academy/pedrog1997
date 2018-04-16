f9281e27b3cdf81ee5ed1d9bff7e5fcd90fd29f5
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-04-16 12:18:35 +0200

    Add frontend

---
+++ week-09/day-01/FrontEnd/FrontEnd/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FrontEnd.Models;
+
+namespace FrontEnd.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        [Route("/")]    
+        public IActionResult Index()
+        {
+            return File("index.html", "text/html");
+        }
+
+        [HttpGet("/doubling")]
+        public IActionResult Doubling([FromQuery]int input)
+        {
+            Models.Double doubling = new Models.Double
+            {
+                Received = input,
+                Result = input * 2
+            };
+            return Json(doubling);
+        }
+    }
+}
---
+++ week-09/day-01/FrontEnd/FrontEnd/Models/Double.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontEnd.Models
+{
+    public class Double
+    {
+        public int Received { get; set; }
+        public int Result { get; set; }
+    }
+}
