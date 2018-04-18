364f90557a81c8f045a2ac62fe10b3274e25952d
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-04-18 20:27:13 +0200

    Add database

---
+++ week-09/day-03/ExampleExam/ExampleExam/Models/SpaceContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ExampleExam.Models
+{
+    public partial class SpaceContext : DbContext
+    {
+        public virtual DbSet<Planet> Planet { get; set; }
+        public virtual DbSet<Spaceship> Spaceship { get; set; }
+
+        public SpaceContext(DbContextOptions<SpaceContext> options) : base(options)
+        { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Space;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Planet>(entity =>
+            {
+                entity.ToTable("planet");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Population).HasColumnName("population");
+            });
+
+            modelBuilder.Entity<Spaceship>(entity =>
+            {
+                entity.ToTable("spaceship");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.MaxCapacity).HasColumnName("max_capacity");
+
+                entity.Property(e => e.Planet)
+                    .HasColumnName("planet")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Utilization).HasColumnName("utilization");
+            });
+        }
+    }
+}
